979af9efe964acc2f35938dda296539d9095068d
micahbaumann <78232761+micahbaumann@users.noreply.github.com>
2024-05-13 00:14:32 -0700

    Update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,47 +1,54 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Input from "../forms/Input";
 import Textarea from "../forms/Textarea";
 import Button from "../forms/Button";
 import "./Contact.css";
-// import database from "./firebase";
-// import { ref, set } from "firebase/database";
-// import axios from "axios";
-
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push } from 'firebase/database';
 
 const Contact = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [sendMsg, setSendMsg] = useState(["", false]);
+
     useEffect(() => {
         document.title = "Contact - Micah Baumann";
     }, []);
 
+    const firebaseConfig = {
+        apiKey: "AIzaSyC80sh4E7yKS7TLMS7pb7Rgx1csu1H6ioM",
+        authDomain: "portfolio-form-c63b3.firebaseapp.com",
+        databaseURL: "https://portfolio-form-c63b3-default-rtdb.firebaseio.com",
+        projectId: "portfolio-form-c63b3",
+        storageBucket: "portfolio-form-c63b3.appspot.com",
+        messagingSenderId: "460401706928",
+        appId: "1:460401706928:web:5c289161c913a045917875"
+    };
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+
     function handleSubmit(e) {
         e.preventDefault();
-        // axios.post("https://portfolio-form-c63b3-default-rtdb.firebaseio.com/contact.json", {
-        //     name: e.target.name.value,
-        //     email: e.target.email.value,
-        //     subject: e.target.subject.value,
-        //     body: e.target.body.value,
-        // }).then(function (response) {
-        //     console.log(response);
-        // })
-        // .catch(function (error) {
-        //     console.log(error);
-        // });
-        
-        // set(ref(database, '/'), {
-        //     name: e.target.name.value,
-        //     email: e.target.email.value,
-        //     subject: e.target.subject.value,
-        //     body: e.target.body.value,
-        // });
-        // const contact = database.ref("contacts").push();
-        // {
-        //     name: e.target.name.value,
-        //     email: e.target.email.value,
-        //     subject: e.target.subject.value,
-        //     body: e.target.body.value,
-        // }
-        // database.ref(`newContacts/${}`)
-        console.log(e.target.name.value);
+        setIsSubmitting(true);
+        if (!e.target.name.value && !e.target.email.value && !e.target.subject.value && !e.target.body.value) {
+            setSendMsg(["Error: Form empty", true]);
+            setIsSubmitting(false);
+        } else {
+            push(ref(database, 'contacts'), {
+                name: e.target.name.value,
+                email: e.target.email.value,
+                subject: e.target.subject.value,
+                body: e.target.body.value,
+            }).then(() => {
+                console.log("submited")
+                setSendMsg(["Sent Successfully", false]);
+                e.target.reset();
+            }).catch((what) => {
+                console.log("error")
+                setSendMsg(["Error Sending", true]);
+            }).finally(() => {
+                setIsSubmitting(false);
+            });
+        }
     }
 
     return (
@@ -49,12 +56,13 @@ const Contact = () => {
             <h1 className="pageTitle">Contact Me</h1>
             <div className="contactForm">
                 <form className="contactForm" onSubmit={handleSubmit}>
-                    <Input type="text" name="name" placeholderLabel="Your Name" className="formElement" />
-                    <Input type="text" name="email" placeholderLabel="Your Email" className="formElement" />
-                    <Input type="text" name="subject" placeholderLabel="Subject" className="formElement" />
-                    <Textarea name="body" className="formElement" innerReize="vertical" placeholderLabel="Subject" rows="7" ></Textarea>
-                    <Button type="submit">Send</Button>
+                    <Input type="text" name="name" placeholderLabel="Your Name" className="formElement" disabled={isSubmitting} />
+                    <Input type="text" name="email" placeholderLabel="Your Email" className="formElement" disabled={isSubmitting} />
+                    <Input type="text" name="subject" placeholderLabel="Subject" className="formElement" disabled={isSubmitting} />
+                    <Textarea name="body" className="formElement" innerReize="vertical" placeholderLabel="Message" rows="7" disabled={isSubmitting} ></Textarea>
+                    <Button type="submit" disabled={isSubmitting}>Send</Button>
                 </form>
+                <p className={`sendMsg ${sendMsg[1] ? "sendError": ""}`}>{sendMsg[0]}</p>
             </div>
         </div>
     );
--- src/components/forms/Button.js
+++ src/components/forms/Button.js
@@ -1,9 +1,25 @@
 import "./form.css";
 
 const Button = (props) => {
+    let name = "";
+    if (props.name) {
+        name = props.name;
+    }
     return (
         <div className={`input ${props.className}`}>
-            <button>{props.children}</button>
+            <button
+                autoFocus={props.autofocus}
+                disabled={props.disabled}
+                form={props.form}
+                formAction={props.formaction}
+                formEncType={props.formenctype}
+                formMethod={props.formMethod}
+                formNoValidate={props.formNoValidate}
+                formTarget={props.formTarget}
+                name={name}
+                type={props.type}
+                value={props.value}
+            >{props.children}</button>
         </div>
     );
 }
