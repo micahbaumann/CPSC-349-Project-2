3eab40c677926fd0d32a503c5178c46dbb4d100c
micahbaumann <micah.ad.mail@gmail.com>
2024-05-13 02:23:49 -0700

    Update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,16 @@
+import "./Home.css"
+
 const Home = () => {
-    return ( <div>home</div> );
+    return (
+        <div className="homeDiv">
+            <div className="innerDiv">
+                <h1>Hi, I'm Micah!</h1>
+                <p className="introText">
+                    I am a computer science student and California State University, Fullerton and Co-Founder & CEO of <a href="https://rutstech.com" target="_blank" rel="noreferrer noopener">Ruts Technologies</a>. I specialize in backend web development with Python and PHP and low-level development in C++. I have developed custom WordPress sites for small businesses and organizations.
+                </p>
+            </div>
+        </div>
+    );
 }
  
 export default Home;
